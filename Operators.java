74bb7cced2d78d8356d388758c211f8f3557352f
LASYACHILLAMCHERLA <2420090011@klh.edu.in>
2025-07-30 14:41:10 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5/2);//2
+		System.out.println(5.0/2);//2.5
+		System.out.println(5/2.0);//2.5
+		System.out.println(5.0/2.0);//2.5
+	}
+
+}
