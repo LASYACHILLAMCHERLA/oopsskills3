5da48eb4cafaecbd1c632912b3536afd95eb007c
LASYACHILLAMCHERLA <2420090011@klh.edu.in>
2025-08-02 11:29:19 +0530

    Add files via upload

---
+++ Sampleswitch.java
@@ -0,0 +1,23 @@
+package week2;
+
+public class Sampleswitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for(int i=0; i<6;i++)
+			 switch(i) 
+			 {
+			 case 0: System.out.println("i is zero."); 
+				break; 
+			case 1: System.out.println("i is one."); 
+				break; 
+			case 2: System.out.println("i is two."); 
+				break; 
+			case 3: System.out.println("i is three.");
+				 break; 
+			default: System.out.println("i is greater than 3."); 
+			 }
+
+	}
+
+}
