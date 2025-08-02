15edd3781584343356a5ba7487bb35856ede0d53
LASYACHILLAMCHERLA <2420090011@klh.edu.in>
2025-08-02 11:28:00 +0530

    Add files via upload

---
+++ Conditionaloperators.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class Conditionaloperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int x=11;
+        System.out.println((x%2==0)?"evem":"odd");
+        }
+
+}
---
+++ Nestedifexample.java
@@ -0,0 +1,25 @@
+
+package week2;
+import java.util.Scanner;
+public class Nestedifexample {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		 System.out.println("Enter an integer: "); 
+		int score = input.nextInt(); 
+        if(score >= 90)
+        	System.out.println("A");
+        else if(score >= 80)
+        	System.out.println("B");
+        else if(score >= 70)
+        	System.out.println("C");
+        else if(score >= 60)
+        	System.out.println("D");
+        else
+        	System.out.println("Fail");
+		
+
+	}
+
+}
